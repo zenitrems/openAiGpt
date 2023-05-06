225a899a1842ec94092b01e39bd0ac0ebb7ef884
Fernando Martinez <thatsthewall@hotmail.com>
2023-05-06 00:14:11 -0500

    script update: Ai personality

--- chatCompletion.py
+++ chatCompletion.py
@@ -3,37 +3,49 @@ import openai
 import dotenv
 
 dotenv.load_dotenv()
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# INSTRUCTION = "Eres Una Ai"
 
-def gpt3Chat():
+with open("text.txt", "r") as INSTRUCTION:
+    content = INSTRUCTION.read()
+
+
+def main():
+    while True:
+        user_input = input("Input: ")
+        chat_ing(user_input)
+
+
+def chat_ing(user_input):
+    while True:
+        response = gpt_response(user_input)
+        print(response)
+
+        action = input("presiona y para generar otra vez: ")
+        if action == "y":
+            response
+        else:
+            return
+
+
+def gpt_response(user_input):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "system",
-                "content": systemRole,
+                "content": content,
             },
             {
                 "role": "user",
-                "content": userRole,
+                "content": user_input,
             },
         ],
-        temperature=0.7,
-        n=1,
-        stop=None,
     )
-    return print(response["choices"][0]["message"]["content"])
+
+    return response["choices"][0]["message"]["content"]
 
 
-systemRole = input("System Content:")
-userRole = input("User Content:")
-
-gpt3Chat()
-do = input("Preguntar de nuevo? (y/n):")
-while do == "y":
-    systemRole = input("System Content:")
-    userRole = input("User Content:")
-    gpt3Chat()
-    do = input("Preguntar de nuevo? (y/n):")
+if __name__ == "__main__":
+    main()
